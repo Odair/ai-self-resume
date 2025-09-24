9a22e3c42c3e58f8fc096e531a6d089e636762ea
Odair <odasm18@gmail.com>
2025-09-23 21:58:57 -0300

    add main and uv setup

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class Myself:
+
+    def __init__(self):
+        self.name= 'Odair'
+
+    def greeting(self):
+        print(f"Hello I'm {self.name} ai-self-resume!")
+
+
+if __name__ == "__main__":
+    mysel = Myself()
+    mysel.greeting()
